c4ed16ff12b3ff5084d48648de5358042abf3cae
Carlin Bingham <cb@viennan.net>
2020-10-31 03:25:05 +0000

    save offset work

--- pos.hs
+++ pos.hs
@@ -5,11 +5,14 @@ import System.IO
 import System.IO.Error
 import Control.Exception (catch)
 import Debug.Trace
+import Data.Binary
+import qualified Data.ByteString.Lazy as B
 
 data LogFile = LogFile
                 { logName :: String
                 , logPath :: String
                 , logImportant :: [String]
+                , logSkip :: Integer
                 }
 
 hasName :: String -> String -> Bool
@@ -49,15 +52,20 @@ eofHandler e
 logHeader :: LogFile -> String
 logHeader l = "=== " ++ logName l ++ " ==="
 
-processLog :: LogFile -> IO ()
+processLog :: LogFile -> IO (Integer)
 processLog l = do
         f <- openFile (logPath l) ReadMode
+        hSeek f AbsoluteSeek (logSkip l)
         putStrLn . logHeader $ l
         processLines f (logImportant l) `catch` eofHandler
         i <- hTell f
-        print i
         hClose f
-        putStrLn "\n\n"
+        putStrLn "\n"
+        return i
+
+printBs :: [Integer] -> IO ()
+printBs offs = print s
+        where s = B.concat $ map encode offs
 
 main :: IO ()
 main = do
@@ -66,12 +74,17 @@ main = do
                         { logName = "log2"
                         , logPath = "log2.log"
                         , logImportant = ["Name1", "Name2"]
+                        , logSkip = 10536777
                         },
                         LogFile
                         { logName = "log1"
                         , logPath = "log1.log"
                         , logImportant = ["Name3"]
+                        , logSkip = 2031063
                         }
                         ]
 
-        mapM_ processLog logFiles
+        offs <- mapM processLog logFiles
+        let bin = B.concat $ map encode offs
+
+        B.writeFile "offsets.dat" bin
